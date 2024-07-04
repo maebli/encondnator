8414ca40489edf381bb1d9d283a38ac1e17e8766
heroichornet <schnipper@gmail.com>
2024-07-04 20:56:18 +0200

    the snake has landed

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
+        .add_systems(Startup, (spawn_snake, setup))
         .add_systems(Update, (print_names, take_damage, despawn_dead).chain())
         .run();
 }
@@ -11,38 +11,20 @@ fn main() {
 #[derive(Debug, Component)]
 struct Health(f32);
 
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.0, 1.0, 0.0);
+
 #[derive(Debug, Component)]
-struct Enemy;
+struct SnakeHead;
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.0, 1.0, 2.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
-
-    // Ground
-    commands.spawn((PbrBundle {
-        mesh: meshes.add(Plane3d::new(Vec3::Y, Vec2::new(1.0, 1.0))),
-        material: materials.add(Color::WHITE),
-        ..default()
-    },));
-
-    // Light
-    commands.spawn(PointLightBundle {
-        transform: Transform::from_xyz(1.0, 2.0, 2.0),
-        point_light: PointLight {
-            shadows_enabled: true,
-            ..default()
-        },
-        ..default()
-    });
+    commands.spawn(Camera2dBundle::default());
 
     commands.spawn((Name::new("J.C. Denton"), Health(100.0)));
-    commands.spawn((Name::new("Bob Page"), Health(200.0), Enemy));
+    commands.spawn((Name::new("Bob Page"), Health(200.0), SnakeHead));
 }
 
 fn print_names(names: Query<(&Name, &Health)>) {
@@ -51,7 +33,7 @@ fn print_names(names: Query<(&Name, &Health)>) {
     }
 }
 
-fn take_damage(mut enemies: Query<&mut Health, With<Enemy>>) {
+fn take_damage(mut enemies: Query<&mut Health, With<SnakeHead>>) {
     for mut health in enemies.iter_mut() {
         health.0 = (health.0 - 10.0).max(0.0);
     }
@@ -63,4 +45,20 @@ fn despawn_dead(mut commands: Commands, healths: Query<(Entity, &Health)>) {
             commands.entity(entity).despawn_recursive();
         }
     }
+}
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
 }
